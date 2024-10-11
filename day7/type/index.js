7a746a1b661edcdf38a219e521cf12eb598beee1
Princewill <princewill.onyekah@kibo.school>
2024-10-11 17:35:26 +0100

    day7

---
+++ day7/type/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var guests = ["Joel", "Lola", "Pelumi", "Kay", "Jide"];
+console.log("I am inviting ".concat(guests.length, " people to dinner."));
+var food_items = ["Cassava", "Garri", "Ewa", "Crayfish"];
+// Join the food items into a comma-separated string
+var message = "I would love to eat ".concat(food_items.join(", "));
+console.log(message);
+var books = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    yearPub: 1937
+};
+console.log(books);
--- day7/type/index.ts
+++ day7/type/index.ts
@@ -8,3 +8,19 @@ var food_items: string[] = ["Cassava", "Garri", "Ewa", "Crayfish"];
 // Join the food items into a comma-separated string
 var message = `I would love to eat ${food_items.join(", ")}`;
 console.log(message);
+
+// Q21 - Then think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+interface Book {
+    title: string;
+    author: string;
+    yearPub: number;
+}
+
+var books: Book = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    yearPub: 1937
+};
+
+console.log(books);
