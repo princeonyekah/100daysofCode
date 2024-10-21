614099168ddc5e942bb2016450352252287440cc
Princewill <princewill.onyekah@kibo.school>
2024-10-21 09:33:42 +0100

    functions

--- day12/index.js
+++ day12/index.js
@@ -37,3 +37,9 @@ var msg = "These animals can fly!"
 
 console.log(msg)
 // Q36 - T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+function make_shirt(size, message) {
+    console.log(`The shirt size is ${size} and the message printed on it is: "${message}"`);
+}
+
+make_shirt(42,"When we vote, we win!")
