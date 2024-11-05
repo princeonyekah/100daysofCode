6ebcc4c1f113dc129ee4a22a935b4265a058c7b0
Princewill <princewill.onyekah@kibo.school>
2024-11-05 15:31:27 +0100

    hasing works

--- NodeJS/auth.ts
+++ NodeJS/auth.ts
@@ -14,6 +14,8 @@ export const createJWT = (user) => {
 const token = jwt.sign({id: user.id,
      username:user.username},
      process.env.JWT_SECRET)
+
+     return token
 }
 
 export const protect = (req, res, next) =>{
