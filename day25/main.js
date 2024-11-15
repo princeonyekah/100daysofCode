f7e47de1baa32c7e5563cccf08a4c90e4a1b3f97
Princewill <princewill.onyekah@kibo.school>
2024-11-15 11:34:26 +0100

    day24+

--- day25/main.js
+++ day25/main.js
@@ -11,4 +11,33 @@ function updateVariable(){
 updateVariable()
 // Question 74: Swapping Variables: Demonstrate how to swap the values of two variables. Start with variables a = 5 and b = 10, then swap their values so that a becomes 10 and b becomes 5.
 
+let a = 5
+let b = 10
+
+let temp = a
+
+a = b
+b = temp
+
+console.log(a)
+console.log(b)
+
 // Question 75: Compound Assignment Operators: Use compound assignment operators to simplify arithmetic operations. Start with x = 4 and perform a series of operations (addition, subtraction, multiplication, division) on x using compound operators.
+
+let x = 4
+
+x += 5
+
+console.log (x)
+x *= 5
+
+console.log (x)
+x/=4
+
+console.log(x)
+
+x -= 7
+
+console.log(x)
+
+
