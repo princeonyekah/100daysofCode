8f88126ca8a12c5cb8d46e0ee998cf7546713689
Princewill <princewill.onyekah@kibo.school>
2024-12-10 20:36:15 +0100

    day40

---
+++ day40/main.js
@@ -0,0 +1,28 @@
+// Question 118: Write a loop that logs numbers from 1 to 10 to the console.
+
+for(let i = 1; i < 11; i++){
+console.log(i)
+}
+
+let i = 1;
+while(i < 11){
+    console.log(i)
+    i++
+}
+// Question 119: Create a while loop that logs "Hello, World!" 5 times.
+
+let num = 0;
+
+while(num < 5){
+    console.log("hello world")
+    num++
+}
+
+
+// Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+const movies = ["Crow", "Stranger Things", "House of Cards"]
+
+for(view of movies){
+    console.log(view)
+}
