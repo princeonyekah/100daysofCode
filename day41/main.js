06ae314b3eceb40106378c2b263a6fd6befd464b
Princewill <princewill.onyekah@kibo.school>
2024-12-12 18:55:02 +0100

    day41_task1

---
+++ day41/main.js
@@ -0,0 +1,14 @@
+// Question 121: Implement a for loop that counts from 1 to 10 but skips number 5 using the continue statement.
+
+for(i = 1;i <= 10; i++){
+
+    if (i === 5) {
+        continue;
+    }
+    console.log(i)
+}
+// Question 122: Use a while loop to count down from 10 to 1 and breaks the loop when it reaches 5.
+
+
+
+// Question 123: Create a loop that iterates through a string and stops when it finds the first vowel.
