c9921b4ca127dabbc54ae66801913cf5791abe27
Princewill <princewill.onyekah@kibo.school>
2025-02-23 10:29:52 +0100

    included vite

--- React_Course/src/App.js
+++ React_Course/src/App.js
@@ -1,21 +1,39 @@
-const Pizza = () => {
+import React from "react"
+import { createRoot } from "react-dom/client"
+
+const Pizza = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "The Pepperoni Pizza"),
-    React.createElement("p", {}, "Mozzarella Cheese Pepperoni"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Padre Gino's"),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
+    React.createElement(Pizza,{
+        name:"The Peperoni Pizza",
+        description:"some dope pizza yo"
+    }),
+    React.createElement(Pizza, {
+        name: "The Hawaiian",
+        description:"The Pineapple and ham, wtf America"
+    }),
+    React.createElement(Pizza,{
+        name:"Chicken Pizza",
+        description:"Chicken nuggies on your pizza."
+    }),
+    React.createElement(Pizza,{
+        name:"Tomato Pizza",
+        description:"Chicken nuggies on your pizza."
+    }),
+    React.createElement(Pizza, {
+        name:"Fish Pizza",
+        description:"Chicken nuggies on your pizza."
+    }),
   ]);
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
